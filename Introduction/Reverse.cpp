cd5b866bcc4e304cdf16275241f472f6c7701586
Goahead15109 <1569521561@qq.com>
2018-06-27 15:05:29 +0800

    Update Reverse.cpp

--- Introduction/Reverse.cpp
+++ Introduction/Reverse.cpp
@@ -1,26 +1,28 @@
 //将一个具有n个元素的数组向左移动i个位置
 
 #include <iostream>
-
 using namespace std; 
-
-void Reverse(int A[],int n)
+void Reverse(int A[],int lo,int hi)
 {
-	int i=0;
-	int j=n-1;
-	while(i<j){
-		int t = A[i];
-		A[i]=A[j];
-		A[j]=t;
-		++i;
-		--j;
+	while(lo<hi){
+		int t = A[lo];
+		A[lo]=A[hi];
+		A[hi]=t;
+		++lo;
+		--hi;
 	}
 } 
+void LeftShift(int A[],int n,int i)
+{
+	Reverse(A,0,i-1);
+	Reverse(A,i,n-1);
+	Reverse(A,0,n-1);
+}
 int main()
 {
 	int A[5]={1,2,3,4,5};
 	
-	Reverse(A,5);
+	LeftShift(A,5,2);
 	for(int i=0;i<5;++i)
 		cout<<A[i]<<" ";
 	
